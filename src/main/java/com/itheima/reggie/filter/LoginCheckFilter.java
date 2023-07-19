387c3a115d5e8797d70b36b293bfa34174e0dc53
Lavinia-shuo <chenshuo_414@163.com>
2023-07-19 15:46:35 +0800

    7.19

--- src/main/java/com/itheima/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/itheima/reggie/filter/LoginCheckFilter.java
@@ -1,6 +1,7 @@
 package com.itheima.reggie.filter;
 
 import com.alibaba.fastjson.JSON;
+import com.itheima.reggie.common.BaseContext;
 import com.itheima.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.AntPathMatcher;
@@ -35,9 +36,11 @@ public class LoginCheckFilter implements Filter{
                 "/employee/login",
                 "/employee/logout",
                 "/backend/**",
-                "/front/**"
+                "/front/**",
+                "/common/**",
+                "/user/sendMsg",
+                "/user/login"
         };
-
 
         //2、判断本次请求是否需要处理
         boolean check = check(urls, requestURI);
@@ -49,9 +52,24 @@ public class LoginCheckFilter implements Filter{
             return;
         }
 
-        //4、判断登录状态，如果已登录，则直接放行
+        //4-1、判断登录状态，如果已登录，则直接放行
         if(request.getSession().getAttribute("employee") != null){
             log.info("用户已登录，用户id为：{}",request.getSession().getAttribute("employee"));
+
+            Long empId = (Long) request.getSession().getAttribute("employee");
+            BaseContext.setCurrentId(empId);
+
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        //4-2、判断登录状态，如果已登录，则直接放行
+        if(request.getSession().getAttribute("user") != null){
+            log.info("用户已登录，用户id为：{}",request.getSession().getAttribute("user"));
+
+            Long userId = (Long) request.getSession().getAttribute("user");
+            BaseContext.setCurrentId(userId);
+
             filterChain.doFilter(request,response);
             return;
         }
